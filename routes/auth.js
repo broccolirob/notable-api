b23d470f00fec3bac8f8d44dc122c50eae635b7c
broccolirob <rschn4911@gmail.com>
2020-03-02 02:10:45 -0500

    Add user model and routes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,65 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const Joi = require("@hapi/joi");
+const { User, validateUser } = require("../models/user");
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
-  return res.status(400).send("Hi! It's your server, bitch. You hit api/auth/");
+  const { error } = validateUser(req);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
+  res.send(true);
 });
 
 router.post("/register", async (req, res) => {
-  console.log(req.body);
-  return res
-    .status(400)
-    .send("Hi! It's your server, bitch. You hit api/auth/register");
+  const { error } = validateUser(req);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("User already registered.");
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 router.post("/access-token", async (req, res) => {
   console.log(req.body);
-  return res
-    .status(400)
-    .send("Hi! It's your server, bitch. You hit api/auth/access-token");
+  return res.status(400).send("Hi! You hit api/auth/access-token");
 });
 
 router.post("/user/update", async (req, res) => {
   console.log(req.body);
-  return res
-    .status(400)
-    .send("Hi! It's your server, bitch. You hit api/auth/user/update");
+  return res.status(400).send("Hi! You hit api/auth/user/update");
 });
+
+function validateAuth(req) {
+  const schema = Joi.object({
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  });
+
+  return schema.validate(user);
+}
 
 module.exports = router;
