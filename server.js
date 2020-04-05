819f5330f91ab518c9fb511a7648b08afff5b38e
broccolirob <rschn4911@gmail.com>
2020-04-05 00:02:23 -0400

    Add debug and config packages.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const cors = require("cors");
 const config = require("config");
+const dbDebug = require("debug")("app:db");
 const auth = require("./routes/auth");
 
 const app = express();
@@ -12,20 +13,40 @@ if (!config.get("jwtPrivateKey")) {
   process.exit(1);
 }
 
+console.log("Application Name: " + config.get("name"));
+console.log("Mail Server: " + config.get("mail.host"));
+console.log("Mail Password: " + config.get("mail.password"));
+
 mongoose
   .connect("mongodb://localhost/notable", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(err => console.error("Could not connect to MongoDB."));
+  .then((conn) => {
+    dbDebug(
+      `Successfully connected to Mongo.
+      Database name: ${conn.connections[0].name}
+      Database host: ${conn.connections[0].host}
+      Database port: ${conn.connections[0].port}\n`
+    );
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    dbDebug(
+      "An error occurred while trying to connect to the database. ",
+      err.message
+    );
+    console.error("Could not connect to MongoDB.");
+  });
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
 app.use("/api/auth", auth);
+
+app.get("/", (req, res) => res.send("Hi there!"));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
